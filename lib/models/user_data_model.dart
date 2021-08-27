350c6bea6d3e22fdba7edda0be98593fb493b06c
ide Shouhi <shouhi0922@icloud.com>
2021-08-27 11:00:15 +0900

    :hammer: test用のuserData

--- lib/models/user_data_model.dart
+++ lib/models/user_data_model.dart
@@ -2,21 +2,14 @@ class UserData {
   final String uid;
   final String displayName;
   final String creationDate;
-  final int avatar;
+  final String avatar;
+  final String about;
 
-  const UserData ({
+  const UserData({
     this.uid,
     this.displayName,
     this.creationDate,
     this.avatar,
+    this.about,
   });
-
-  factory UserData.fromJson(Map<String, dynamic> json){
-    return UserData(
-      uid: json['UID'].toString(),
-      displayName: json['DisplayName'].toString(),
-      creationDate: json['CreationDate'].toString(),
-      avatar: json['Avatar'] as int,
-    );
-  }
 }
