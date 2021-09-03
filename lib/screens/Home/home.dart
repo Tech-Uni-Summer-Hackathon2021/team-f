0d5adb3f0c53a2d186762f03ad0545b289bdcfe9
Shimizu-Yuuki <82706827+Shimizu-Yuuki@users.noreply.github.com>
2021-09-03 17:07:28 +0900

    list_add_delete_second

--- lib/screens/Home/home.dart
+++ lib/screens/Home/home.dart
@@ -1,7 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:task_project/screens/Home/list_add.dart';
 import 'package:task_project/widgets/widget.dart';
-//import package:task_project/widgets/list_add.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class TodoListPage extends StatefulWidget {
   @override
@@ -9,11 +10,23 @@ class TodoListPage extends StatefulWidget {
 }
 
 class _TodoListPageState extends State<TodoListPage> {
-  List<String> todoList = [];
+  List todoList = [
+    {
+      "title": "aaa",
+      "dl": "",
+      "isExpired": true,
+    },
+    {"title": "bbb", "dl": "", "isExpired": false},
+    {"title": "ccc", "dl": "", "isExpired": false}
+  ];
+  var workingIndex = -1;
+
+  get dl => null;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(int.parse("0xfff0ffff")),
       appBar: AppbarMain(
         title: Text("タスクリスト"),
         isAction: true,
@@ -23,22 +36,70 @@ class _TodoListPageState extends State<TodoListPage> {
         itemCount: todoList.length,
         itemBuilder: (context, index) {
           return Card(
+              child: Slidable(
+            actionPane: SlidableStrechActionPane(),
+            actionExtentRatio: 0.2,
+            actions: [
+              IconSlideAction(
+                caption: 'Complete!',
+                icon: Icons.assignment_turned_in_outlined,
+                color: Colors.lightGreen,
+                foregroundColor: Colors.white,
+              )
+            ],
+            secondaryActions: [
+              IconSlideAction(
+                caption: 'Delete',
+                icon: Icons.delete,
+                color: Colors.red,
+                foregroundColor: Colors.white,
+                onTap: () => setState(() => todoList.removeAt(index)),
+              )
+            ],
             child: ListTile(
-              title: Text(todoList[index]),
+              selected: (index == workingIndex) ? true : false,
+              selectedTileColor: Colors.greenAccent,
+              leading: Icon(
+                Icons.check_circle_outline,
+                color: Colors.black12,
+                size: 24,
+              ),
+              title: Text(
+                todoList[index]['title'],
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+                style: TextStyle(fontSize: 20),
+              ),
+              subtitle: Text(
+                'MM/DD n時ｍｍ分  x Day ${(todoList[index]['isExpired']) ? 'past' : 'left'}',
+                textAlign: TextAlign.end,
+                style: TextStyle(
+                  color: (todoList[index]['isExpired'])
+                      ? Colors.pinkAccent
+                      : Colors.black54,
+                ),
+              ),
+              onTap: () => setState(() => workingIndex = index),
+              onLongPress: () => setState(() {
+                if (workingIndex == index) {
+                  workingIndex = -1;
+                }
+              }),
+              contentPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 20),
             ),
-          );
+          ));
         },
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
-          final newListText = await Navigator.of(context).push(
+          final title = await Navigator.of(context).push(
             MaterialPageRoute(builder: (context) {
               return TodoAddPage();
             }),
           );
-          if (newListText != null) {
+          if (title != null) {
             setState(() {
-              todoList.add(newListText);
+              todoList.add({title: 'title', 'dl': '', 'isExpired': false});
             });
           }
         },
