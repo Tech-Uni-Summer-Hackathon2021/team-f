c34d0cc364fd23c7036a1204d861c0eaa7fb7ad8
Shimizu-Yuuki <82706827+Shimizu-Yuuki@users.noreply.github.com>
2021-09-04 07:46:06 +0900

    list_add_memo_picker

--- lib/screens/Home/list_add.dart
+++ lib/screens/Home/list_add.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
+import 'package:intl/intl.dart';
 
 class TodoAddPage extends StatefulWidget {
   @override
@@ -6,51 +8,100 @@ class TodoAddPage extends StatefulWidget {
 }
 
 class _TodoAddPageState extends State<TodoAddPage> {
-  String _text = '';
+  String _title = '';
+  DateTime _dlOb = DateTime.now();
+  String _dl = '';
+  String _memo = '';
+
+  var formatter = new DateFormat('yyyy/MM/dd HH:mm');
 
   @override
   Widget build(BuildContext context) {
+    _dl = formatter.format(_dlOb);
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text('リスト追加'),
-      ),
-      body: Container(
-        padding: EdgeInsets.all(64),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(_text, style: TextStyle(color: Colors.blue)),
-            const SizedBox(height: 8),
+        appBar: AppBar(
+          title: Text('リスト追加'),
+        ),
+        body: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                IconButton(
+                  icon: Icon(Icons.close),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                ),
+                Visibility(
+                  visible: _title != '',
+                  maintainSize: true,
+                  maintainAnimation: true,
+                  maintainState: true,
+                  child: ElevatedButton(
+                    child: Text('作成'),
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.cyan,
+                      onPrimary: Colors.white,
+                      shape: StadiumBorder(),
+                    ),
+                    onPressed: () {
+                      //print(DateTime.now().toLocal().timeZoneName);
+                      if (_title != '') {
+                        Navigator.of(context).pop({
+                          'title': _title,
+                          'dl': _dl,
+                          'dlOb': _dlOb,
+                          'memo': _memo
+                        });
+                      }
+                    },
+                  ),
+                )
+              ],
+            ),
             TextField(
+              autofocus: true,
+              decoration: InputDecoration(
+                hintText: 'タスクを入力してください',
+                border: InputBorder.none,
+              ),
               onChanged: (String value) {
                 setState(() {
-                  _text = value;
+                  _title = value;
                 });
               },
             ),
-            const SizedBox(height: 8),
-            Container(
-              width: double.infinity,
-              child: ElevatedButton(
-                onPressed: () {
-                  Navigator.of(context).pop(_text);
-                },
-                child: Text('リスト追加', style: TextStyle(color: Colors.white)),
-              ),
+            TextField(
+              decoration: InputDecoration(border: InputBorder.none),
+              controller: TextEditingController(text: _dl),
+              readOnly: true,
+              onTap: () {
+                DatePicker.showDateTimePicker(context, currentTime: _dlOb,
+                    onConfirm: (date) {
+                  setState(() {
+                    _dlOb = date;
+                  });
+                });
+              },
             ),
-            const SizedBox(height: 8),
-            Container(
-              width: double.infinity,
-              child: TextButton(
-                onPressed: () {
-                  Navigator.of(context).pop();
+            Expanded(
+              child: TextField(
+                decoration: InputDecoration(
+                    hintText: 'メモ',
+                    hintStyle: TextStyle(),
+                    border: InputBorder.none),
+                maxLines: null,
+                expands: false,
+                onChanged: (String value) {
+                  setState(() {
+                    _memo = value;
+                  });
                 },
-                child: Text('キャンセル'),
               ),
-            ),
+            )
           ],
-        ),
-      ),
-    );
+        ));
   }
 }
